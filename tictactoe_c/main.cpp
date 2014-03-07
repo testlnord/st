808b5dc99403611bbb2edc32abeac8ec6a05905a
testlnord <testlnord@gmail.com>
2014-03-07 10:31:11 +0400

    c tictactoe beautifying

--- tictactoe_c/main.cpp
+++ tictactoe_c/main.cpp
@@ -12,24 +12,31 @@ using std::cin;
 using std::endl;
 
 const int FLDSIZE = 3;
+const int X_MARK = 1;
+const int O_MARK = -1;
+const char X_SYM = 'x';
+const char O_SYM = 'o';
+
 
 int whos_turn(const vector<char>  & field){
-	int x = std::count(field.begin(), field.end(), 'x');
-	int o = std::count(field.begin(), field.end(), 'o');
+	int x = std::count(field.begin(), field.end(), X_SYM);
+	int o = std::count(field.begin(), field.end(), O_SYM);
 	if (x < o){
-		return 1;
+		return X_MARK;
 	}else{
-		return -1;
+		return O_MARK;
 	}
 }
 
 pair<int, int> turn(const vector<char> & field, int turn){
 	int potential[FLDSIZE*FLDSIZE];
 	std::fill(potential, potential + FLDSIZE*FLDSIZE, 0);
+	int best = -1; //initialize
+
 	for (int i = 0 ; i < FLDSIZE; ++i){
 		for (int j = 0; j < FLDSIZE; ++j){
 			char c = field[i*FLDSIZE + j];
-			int m = c=='x'?1:(c=='o'?-1:0);
+			int m = c==X_SYM?X_MARK:(c==O_SYM?O_MARK:0);
 			if (m != 0){
 				for (int l = 0; l < FLDSIZE; ++l)
 					potential[l*FLDSIZE + j] += m;
@@ -41,17 +48,20 @@ pair<int, int> turn(const vector<char> & field, int turn){
 				}
 				if (i + j == FLDSIZE - 1){
 					for (int l = 0; l < FLDSIZE; ++l)
-						potential[l*FLDSIZE +FLDSIZE -1 - l] += m;
+						potential[l*FLDSIZE +FLDSIZE - 1 - l] += m;
 				}
+			}else{
+				best = i*FLDSIZE + j;
 			}
 		}
 	}
-	int best = 0;
+
 	for (int i = 0; i < FLDSIZE*FLDSIZE; i++)
-		if (field[i] != 'x' && field[i] != 'o')
-		if ((turn > 0 && potential[best] < potential[i])||
-			(turn < 0 && potential[best] > potential[i]))
+		if (field[i] != X_SYM && field[i] != O_SYM)
+		if ((turn == X_MARK && potential[best] < potential[i])||
+			(turn == O_MARK && potential[best] > potential[i]))
 			best = i;
+	cout << field[best] << endl;
 	return std::make_pair(best/FLDSIZE, best%FLDSIZE);
 }
 
