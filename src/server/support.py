f024868862c76dab22b5071c9a6ce2f89cdfd6b5
s <stanisvs@gmail.com>
2014-05-19 08:48:07 +0400

    zmq server, refactor support

--- src/server/support.py
+++ src/server/support.py
@@ -14,15 +14,16 @@ import sqlite3
 supportedHandlers = {}
 
 
-def jsonify( data):
+def jsonify(data):
     jsonData = json.dumps(data)
     return jsonData.encode("utf-8")
 
 
 def support(key):
     def adder(method):
-        supportedHandlers[key]=method
+        supportedHandlers[key] = method
         return method
+
     return adder
 
 
@@ -34,105 +35,109 @@ def make_out_path(tour_id, user_id):
     return res_path
 
 
-def make_runner (sollution,timelimit):
-     r = util.register.runners[sollution["runner_name"]](
-                    os.path.join(make_out_path(sollution["tour_id"], sollution["user_id"]), sollution["file_name"]),
-                    timelimit)
-     return r
+def make_runner(sollution, timelimit):
+    r = util.register.runners[sollution["runner_name"]](
+        os.path.join(make_out_path(sollution["tour_id"], sollution["user_id"]), sollution["file_name"]),
+        timelimit)
+    return r
+
 
 #DECORATORS
 @support("/add_user")
-def addUser(db,data,path):
+def addUser(db, data, path):
+    data = get_dict_from_json_data(data)
+    name = data["name"]
+    email = data["email"]
 
-        data=get_dict_from_json_data(data)
-        name = data["name"]
-        email= data["email"]
-
-        # try:
-        db.addUser(name,email)
-        dict=["user "+name+" added"]
-        return jsonify(dict)
+    # try:
+    db.addUser(name, email)
+    dict = ["user " + name + " added"]
+    return jsonify(dict)
 
 
 @support("/get_user_info")
-def getUserInfo(db,path):
-    dict=parsePath(path)
+def getUserInfo(db, path):
+    dict = parsePath(path)
 
     if "id" in dict:
-        data=db.getUser(dict["id"])
+        data = db.getUser(dict["id"])
     else:
-        data=db.getUser(None,dict["name"])
-    return jsonify(data)
-
-@support ("/get_tournaments")
-def get_tournaments(db,path):
-    data=db.getTournament()
+        data = db.getUser(None, dict["name"])
     return jsonify(data)
 
 
-@support ("/get_checkers")
-def get_checkers (db,path):
+@support("/get_tournaments")
+def get_tournaments(db, path):
+    data = db.getTournament()
+    return jsonify(data)
+
+
+@support("/get_checkers")
+def get_checkers(db, path):
     data = list(util.register.checkers.keys())
     print(data)
     jsonData = json.dumps(data)
     return jsonData.encode("utf-8")
 
-@support ("/get_builders")
-def get_builders (db,path):
+
+@support("/get_builders")
+def get_builders(db, path):
     data = list(util.register.builders.keys())
     print(data)
     jsonData = json.dumps(data)
     return jsonData.encode("utf-8")
 
+
 @support("/create_tournament")
-def addTournament(db,data,path):
-    data=get_dict_from_json_data(data)
+def addTournament(db, data, path):
+    data = get_dict_from_json_data(data)
     name = data["name"]
-    checker= data["checker"]
+    checker = data["checker"]
     timelimit = data["timelimit"]
-    start_time = data ["start_time"]
-    end_time  = data ["end_time"]
-    db.addTournament(name,checker,timelimit,start_time,end_time)
+    start_time = data["start_time"]
+    end_time = data["end_time"]
+    db.addTournament(name, checker, timelimit, start_time, end_time)
 
 
-@support ("/add_participant")
-def add_participant(db,data,path):
+@support("/add_participant")
+def add_participant(db, data, path):
     data = get_dict_from_json_data(data)
     if not data:
         return
     name = data["name"]
     t_name = data["tournament_name"]
-    db.addParticipantByNames(t_name,name)
+    db.addParticipantByNames(t_name, name)
 
 
-@support ("/send_solution")
-def add_solution (db,data,path):
-    dict=parsePath(path)
-    name=dict["name"]
-    t_name=dict["tournament_name"]
-    type=dict["type"]
+@support("/send_solution")
+def add_solution(db, data, path):
+    dict = parsePath(path)
+    name = dict["name"]
+    t_name = dict["tournament_name"]
+    type = dict["type"]
     return jsonify(addBuild(db, name, t_name, data, type))
 
 
-@support ("/run_tournament")
-def run_tournament (db,path):
-    dict=parsePath(path)
+@support("/run_tournament")
+def run_tournament(db, path):
+    dict = parsePath(path)
     tour_name = dict["tournament_name"]
     tour_info = db.getTournament(name=tour_name)[0]
-    run_by_tinfo(db,tour_info)
+    run_by_tinfo(db, tour_info)
     # create_run(db,dict["tournament_name"])
     return None
 
-@support ("/get_run_result")
-def get_run_result (db,path):
+
+@support("/get_run_result")
+def get_run_result(db, path):
     dict = parsePath(path)
     data = db.getRunResult(dict["tour_name"])
     jsonData = json.dumps(data)
     return jsonData.encode("utf-8")
 
 
-@support ("/get_user_tour_info")
-def get_run_result (db,path):
+@support("/get_user_tour_info")
+def get_run_result(db, path):
     dict = parsePath(path)
     data = db.getUserTours(dict["user_name"])
     jsonData = json.dumps(data)
@@ -140,55 +145,56 @@ def get_run_result (db,path):
 
 
 @support("/check_user_in_tour")
-def checkUserInTournament (db,path):
-     dict=parsePath(path)
-     data = db.checkUserInTour(dict['name'],dict['t_name'])
-     jsonData = json.dumps(data)
-     return jsonData.encode("utf-8")
+def checkUserInTournament(db, path):
+    dict = parsePath(path)
+    data = db.checkUserInTour(dict['name'], dict['t_name'])
+    jsonData = json.dumps(data)
+    return jsonData.encode("utf-8")
 
 
-@support ("/run_active_tours")
-def run_active_tours (db,path):
+@support("/run_active_tours")
+def run_active_tours(db, path):
     tinfos = db.get_active_tours()
     for t in tinfos:
-        run_by_tinfo(db,t)
-
-def run_by_tinfo (db,tinfo):
-     tid = tinfo["id"]
-     timelimit = tinfo["tl"]
-     run_id = db.addRun(tid, str(datetime.datetime.now()))
-     solutions = db.getSolutionsInTournament(tid)
-
-     for sol1_num, sol1 in enumerate(solutions[:-1]):
-            for sol2 in solutions[sol1_num + 1:]:
-                r1 = make_runner(sol1,timelimit)
-                r2 = make_runner(sol2,timelimit)
-
-                #first time
-                checker = util.register.checkers[tinfo["c"]](r1, r2)
-                checker.play()
-                p1, p2 = checker.points()
-                db.addGame(run_id, sol1["id"], sol2["id"], p1, p2, checker.log())
-                #second time
-                checker = util.register.checkers[tinfo["c"]](r2, r1)
-                checker.play()
-                p1, p2 = checker.points()
-                db.addGame(run_id, sol2["id"], sol1["id"], p1, p2, checker.log())
+        print("run ", t)
+        run_by_tinfo(db, t)
 
 
+def run_by_tinfo(db, tinfo):
+    tid = tinfo["id"]
+    timelimit = tinfo["tl"]
+    run_id = db.addRun(tid, str(datetime.datetime.now()))
+    solutions = db.getSolutionsInTournament(tid)
+
+    for sol1_num, sol1 in enumerate(solutions[:-1]):
+        for sol2 in solutions[sol1_num + 1:]:
+            r1 = make_runner(sol1, timelimit)
+            r2 = make_runner(sol2, timelimit)
+
+            #first time
+            checker = util.register.checkers[tinfo["c"]](r1, r2)
+            checker.play()
+            p1, p2 = checker.points()
+            print(p1, p2)
+            db.addGame(run_id, sol1["id"], sol2["id"], p1, p2, checker.log())
+            #second time
+            checker = util.register.checkers[tinfo["c"]](r2, r1)
+            checker.play()
+            p1, p2 = checker.points()
+            print(p1, p2)
+            db.addGame(run_id, sol2["id"], sol1["id"], p1, p2, checker.log())
 
 
 def create_run(db, tour_name):
-        tour_info = db.getTournament(name=tour_name)[0]
-        tour_id = tour_info["id"]
+    tour_info = db.getTournament(name=tour_name)[0]
+    tour_id = tour_info["id"]
 
-        run_id = db.addRun(tour_id,str(datetime.datetime.now()))
-        solutions = db.getSolutionsInTournament(tour_id)
+    run_id = db.addRun(tour_id, str(datetime.datetime.now()))
+    solutions = db.getSolutionsInTournament(tour_id)
 
-        for sol1_num, sol1 in enumerate(solutions[:-1]):
-            for sol2 in solutions[sol1_num + 1:]:
-                db.addGame(run_id, sol2["id"], sol1["id"], 0, 0, 'NOT PLAYED')
-
+    for sol1_num, sol1 in enumerate(solutions[:-1]):
+        for sol2 in solutions[sol1_num + 1:]:
+            db.addGame(run_id, sol2["id"], sol1["id"], 0, 0, 'NOT PLAYED')
 
 
 def addBuild(db, user_name, tour_name, file, builder_name):
@@ -204,7 +210,7 @@ def addBuild(db, user_name, tour_name, file, builder_name):
 
     fileparams = 'w'
     if type(file) == bytes:
-        fileparams+='b'
+        fileparams += 'b'
     with open(src_path, fileparams) as src_file:
         src_file.write(file)
     b_stat = 0
@@ -220,20 +226,22 @@ def addBuild(db, user_name, tour_name, file, builder_name):
     db.addSolution(user_info["id"], tour_info["id"], b_stat, str(datetime.datetime.now()), builder_name, o_file)
     return result
 
+
 def parsePath(path):
-    s=path.split("?")[1]
-    chunks=s.split(",")
-    dict={}
+    s = path.split("?")[1]
+    chunks = s.split(",")
+    dict = {}
     for chunk in chunks:
-        key,value= chunk.split("=")
-        dict[key]=value
+        key, value = chunk.split("=")
+        dict[key] = value
     return dict
 
-def get_dict_from_json_data (json_post_data):
+
+def get_dict_from_json_data(json_post_data):
     # return json_post_data
     print(json_post_data)
-    post_data = urllib.parse.parse_qs(json_post_data,keep_blank_values=1)
-    json_data=next(iter(post_data.keys()))
+    post_data = urllib.parse.parse_qs(json_post_data, keep_blank_values=1)
+    json_data = next(iter(post_data.keys()))
     json_dict = json.loads(json_data)
     return json_dict
 
---
+++ src/server/zmq_server.py
@@ -0,0 +1,117 @@
+import json
+import zmq
+import sys
+import threading
+import time
+from random import randint, random
+import db
+
+
+from config import num_of_server_threads
+from config import serverPort
+
+__author__ = "s"
+
+
+def tprint(msg):
+    """like print, but won't get newlines confused with multiple threads"""
+    sys.stdout.write(msg + '\n')
+    sys.stdout.flush()
+
+class ClientTask(threading.Thread):
+    """ClientTask"""
+    def __init__(self, id):
+        self.id = id
+        threading.Thread.__init__ (self)
+
+    def run(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.DEALER)
+        identity = u'worker-%d' % self.id
+        socket.identity = identity.encode('utf-8')
+        socket.connect('tcp://localhost:%s',serverPort)
+        print('Client %s started' % (identity))
+        poll = zmq.Poller()
+        poll.register(socket, zmq.POLLIN)
+        reqs = 0
+        while True:
+            reqs = reqs + 1
+            print('Req #%d sent..' % (reqs))
+            socket.send_string(u'request #%d' % (reqs))
+            for i in range(5):
+                sockets = dict(poll.poll(1000))
+                if socket in sockets:
+                    msg = socket.recv()
+                    tprint('Client %s received: %s' % (identity, msg))
+        socket.close()
+        context.term()
+
+class ServerTask(threading.Thread):
+    """ServerTask"""
+    def __init__(self):
+        threading.Thread.__init__ (self)
+
+    def run(self):
+        context = zmq.Context()
+        frontend = context.socket(zmq.ROUTER)
+        frontend.bind('tcp://*:%s',serverPort)
+
+        backend = context.socket(zmq.DEALER)
+        backend.bind('inproc://backend')
+
+        workers = []
+        for i in range(num_of_server_threads):
+            worker = ServerWorker(context)
+            worker.start()
+            workers.append(worker)
+
+        poll = zmq.Poller()
+        poll.register(frontend, zmq.POLLIN)
+        poll.register(backend,  zmq.POLLIN)
+
+        while True:
+            sockets = dict(poll.poll())
+            if frontend in sockets:
+                ident, msg = frontend.recv_multipart()
+                tprint('Server received %s id %s' % (msg, ident))
+                backend.send_multipart([ident, msg])
+            if backend in sockets:
+                ident, msg = backend.recv_multipart()
+                tprint('Sending to frontend %s id %s' % (msg, ident))
+                frontend.send_multipart([ident, msg])
+
+        frontend.close()
+        backend.close()
+        context.term()
+
+class ServerWorker(threading.Thread):
+    """ServerWorker"""
+    def __init__(self, context):
+        threading.Thread.__init__ (self)
+        self.db = db.DB()
+        self.context = context
+
+    def run(self):
+        worker = self.context.socket(zmq.DEALER)
+        worker.connect('inproc://backend')
+        tprint('Worker started')
+        while True:
+            ident, msg = worker.recv_multipart()
+            tprint('Worker received %s from %s' % (msg, ident))
+            json_dict = json.loads(msg.decode("utf-8"))
+            data = supportedHandlers[json_dict["key"]](self.db, json_dict)
+            worker.send_multipart([ident, data])
+        worker.close()
+
+def main():
+    """main function"""
+    server = ServerTask()
+    server.start()
+    for i in range(3):
+        client = ClientTask(i)
+        client.start()
+
+    server.join()
+
+if __name__ == "__main__":
+    main()
