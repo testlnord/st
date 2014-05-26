b50e58a38a92f677f2c44b7c8b60575cc312e8dc
s <stanisvs@gmail.com>
2014-05-26 10:33:55 +0400

    fix java vot

---
+++ test/tictactoe/java/Main.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static int mark = 1;
+
+    public static int[][] field = new int[3][3];
+
+    public static void main(String[] arg) throws IOException {
+        readInput();
+        makeTurn();
+    }
+
+    private static void readInput() throws IOException {
+        String input;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        input = in.readLine();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                switch (input.charAt(3*i + j)) {
+                    case 'x':
+                        field[i][j] = 1;
+                        break;
+                    case 'o':
+                        field[i][j] = -1;
+                        break;
+                    default:
+                        field[i][j] = 0;
+                        break;
+                }
+            }
+        }
+
+        if (sum() > 0) {
+            mark = -1;
+        } else {
+            mark = 1;
+        }
+
+    }
+
+    private static void makeTurn() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (field[i][j] == 0) {
+                    System.out.println(String.valueOf(i)+" "+String.valueOf(j));
+                    field[i][j] = mark;
+                    return;
+                }
+            }
+        }
+    }
+
+    private static int sum() {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sum += field[i][j];
+            }
+        }
+        return sum;
+    }
+    private static void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                switch (field[i][j]) {
+                    case 1:
+                        System.out.print('x');
+                        break;
+                    case -1:
+                        System.out.print('o');
+                        break;
+                    default:
+                        System.out.print('-');
+                        break;
+                }
+            }
+        }
+    }
+}
